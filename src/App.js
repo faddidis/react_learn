82942a2dbf8b12603d98d54672aa00435029f249
faddidis <faddidis@gmail.com>
2022-11-25 01:32:59 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 function App() {
@@ -11,12 +13,36 @@ function App() {
     {id: 1, title: 'Javascript', body: 'Description'},
     {id: 2, title: 'Javascript 2', body: 'Description'},
     {id: 3, title: 'Javascript 3', body: 'Description'},
-    {id: 4, title: 'Javascript 4', body: 'Description'},
-    {id: 5, title: 'Javascript 5', body: 'Description'},
     ])
+    const [post, setPost] = useState( {title: '', body: ''})
+
+    const addNewPost = (e) => {
+      e.preventDefault();
+       
+      setPosts( [ ...posts, {...post, id: Date.now()}])
+      setPost( {title: '', body: ''})
+    }
   return (
-    <div className="App">
-      <Post/>
+    <div className="App"> 
+      <form>
+        {/*уПРАВЛЯЕМЫЙ КОМПОНЕНТ*/}
+        <MyInput 
+            value={post.title}
+            onChange={e => setPost( {...post, title: e.target.value})}
+            type="text"
+            placeholder="Название поста"
+          />
+          {/* Неконторлируемый компонент */}
+        <MyInput
+            value={post.body}
+            onChange={e => setPost( {...post, body: e.target.value})}
+            type="text" 
+            placeholder="Описание поста"
+        />
+        
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+        <PostList posts={posts} title="Посты про JS"/>
+      </form>
     </div>  
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -4,11 +4,12 @@ const PostItem = (props) => {
     return (
         <div className="post">
         <div className='post__content'>
-          <strong>{props.post.id}. {props.post.title}</strong>
+          <strong>{props.number}. {props.post.title}</strong>
           <div> 
-          {props.post.body}
+          {props.post.body} 
           </div>
           <div className='post__btns'>
+            
               <button>Удалить</button>
           </div>
         </div>
